6a4663468ffa601a2a0c668e7fb8fd9730846681
Gabriel Rizzo <daudtrizzo@gmail.com>
2021-04-04 19:30:24 -0300

    responsividade do menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,30 @@
-// var slideIndex = 0;
-// showSlides();
+var slideIndex = 0;
+showSlides();
 
-// function showSlides() {
-//     var i;
-//     var slides = document.getElementsByClassName("slide");
-//     var dots = document.getElementsByClassName("pointer");
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//     }
-//     slideIndex++;
-//     if (slideIndex > slides.length) { slideIndex = 1 }
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].className = dots[i].className.replace(" active", "");
-//     }
-//     slides[slideIndex - 1].style.display = "block";
-//     dots[slideIndex - 1].className += " active";
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("slide--banner");
+    var dots = document.getElementsByClassName("pointer--banner");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    console.log(slides.length)
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 
-//     setTimeout(showSlides, 8000)
-// }
+    setTimeout(showSlides, 2000)
+}
+
+function MenuOpener() {
+    let button = document.getElementById('menu--action')
+    if (button.style.display == 'none') {
+        button.style.display = 'block'
+    } else {
+        button.style.display = 'none'
+    }
+}
